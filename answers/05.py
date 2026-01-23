9666a298cdd610c0d5a7cbc306d27ee299c21a1f
Ravindu Weerasinghe <ravindutharuka2001@gmail.com>
2026-01-24 00:05:04 +0530

    Add kernel visualization

--- answers/05.py
+++ answers/05.py
@@ -12,4 +12,18 @@ def main():
     kernal_5x5 = get_normalized_gaussian_kernal(5, 2) 
     print(f"5x5 Gaussian Kernal (sigma=2): \n{np.round(kernal_5x5, 3)}")
 
+    kernel_51x51 = get_normalized_gaussian_kernal(51, 10)
+
+    # grid
+    ax = np.linspace(-25, 25, 51)
+    Y, X = np.meshgrid(ax, ax)
+
+    fig = plt.figure(figsize=(8, 6))
+    ax1 = fig.add_subplot(111, projection='3d')
+    ax1.plot_surface(X, Y, kernel_51x51, cmap='viridis')
+    ax1.set_title("3D Surface of 51x51 Gaussian Kernel")
+    ax1.set_zlabel("Height (Coefficient)")
+
+    plt.tight_layout()
+    plt.show()
 main()
