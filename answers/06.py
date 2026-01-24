e41de580b8cfe2997bfed29eac40eb9827986052
Ravindu Weerasinghe <ravindutharuka2001@gmail.com>
2026-01-24 09:59:32 +0530

    Add DoG 3D visualization

--- answers/06.py
+++ answers/06.py
@@ -8,7 +8,7 @@ def get_derivative_of_gaussian(kernel_size: int, sigma: float) -> Tuple[np.ndarr
     Y, X = np.meshgrid(ax, ax)
     standard_gaussian = np.exp(-(X**2 + Y**2) / (2 * sigma**2)) # constant factor omitted since we normalize later
 
-    # DoG: dG/dZ = - (Z / sigma^2) * G(Z)
+    # DoG: dG/dZ = - (Z / sigma^2) * G(.)
     kernel_X = - (X / (sigma**2)) * standard_gaussian
     kernel_Y = - (Y / (sigma**2)) * standard_gaussian
     
@@ -22,4 +22,16 @@ def main():
     print(f"5x5 DoG Kernel (sigma=2) - X direction: \n{np.round(kernel_5x5_X, 3)}")
     print(f"5x5 DoG Kernel (sigma=2) - Y direction: \n{np.round(kernel_5x5_Y, 3)}")
 
+    kernel_51x51_X, kernel_51x51_Y = get_derivative_of_gaussian(51, 8)
+    grid = np.linspace(-25, 25, 51)
+    X, Y = np.meshgrid(grid, grid)
+    fig = plt.figure(figsize=(18, 10))
+
+    ax1 = fig.add_subplot(1, 1, 1, projection='3d')
+    ax1.plot_surface(X, Y, kernel_51x51_X, cmap='coolwarm')
+    ax1.set_title("Derivative of Gaussian (X)")
+    ax1.set_zlabel("Amplitude")
+
+    plt.tight_layout()
+    plt.show()
 main()
